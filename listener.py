a9eb5bc7bd1890aff44d4a56386c0c7bf4e4dce9
Robert Chapman <rschapman@gmail.com>
2013-04-02 11:38:53 -0500

    Initial Commit
    This is just some boilerplate comments and code to begin the project.
    Awaiting clarification of feature set.

---
+++ listener.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import MySQLdb
+
+#database connection
+#fill in the blanks in production
+db = MySQLdb.connect(host = "", user = "", passwrd = "", dbname = "") 
+
+#cursor class necessary for queries in Python
+cur = db.cursor()
+
+
+#keywords
+
+#Google News Listener
+
+#Twitter Listener
+
